a5fa46c4826b5ff27efc49177bc14c83798a7e49
Robert <rtedwards.eng@gmail.com>
2020-05-16 13:16:08 +0100

    cleaned up linting

--- DataBase/CSVDataBase.py
+++ DataBase/CSVDataBase.py
@@ -24,7 +24,7 @@ class CSVDataBase(DataBase):
         '''Loads data from CSV files'''
         pass
 
-    def _get_csv_from_url(url,outfile):
+    def _get_csv_from_url(self, url, outfile):
         """
         download a csv from a url
         """
@@ -33,7 +33,7 @@ class CSVDataBase(DataBase):
         urllib.request.urlretrieve(url, filepath)
         return
 
-    def _get_wb_data(file_path,data_name):
+    def _get_wb_data(self, file_path, data_name):
         #http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv
         df = pd.read_csv(file_path,skiprows=4)
         series = df.set_index('Country Name')['2018']
@@ -49,53 +49,57 @@ class CSVDataBase(DataBase):
         
         return data
 
-    def _join_wb_data():
+    def _join_wb_data(self):
         #get pop density
         #http://api.worldbank.org/v2/en/indicator/EN.POP.DNST?downloadformat=csv
         pop_density = get_wb_data('API_EN.POP.DNST_DS2_en_csv_v2_936296.csv','pop_density')
+
         #get population
         #http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv
-        pop = get_wb_data('API_SP.POP.TOTL_DS2_en_csv_v2_936048.csv','pop')
+        pop_total = get_wb_data('API_SP.POP.TOTL_DS2_en_csv_v2_936048.csv','pop')
         
-        wb_data = pd.concat([pop_density,pop],axis=1)
+        wb_data = pd.concat([pop_density, pop_total], axis=1)
         return wb_data
 
-    def _join_data_sources():
+    def _join_data_sources(self):
         df = scraper.get_latest_data()
         df = df.join(join_wb_data())
         return df
 
-    def _generate_features(df):
+    def _generate_features(self, df):
         df['deaths_per_million'] = df['deaths'] / df['pop'] * 1000000
         df['cases_per_million'] = df['cases'] / df['pop'] * 1000000
         #calculate additional metrics
         df['death_rate_%'] = df['deaths'] / df['cases'] * 100
         return df
-        
-    def _import_static_data():
+
+    def _import_static_data(self):
         df = join_data_sources()
         df = generate_features(df)
         return df
 
-    def _get_csse_time_series_deaths():
+    def _get_csse_time_series_deaths(self):
         df = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
         meta_cols = ['Province/State','Country/Region','Lat','Long']
-        time_series = df.drop(meta_cols,axis=1)
-        #get a unique country name to use as id
+        time_series = df.drop(meta_cols, axis=1)
+
+        # get a unique country name to use as id
         df['Province/State'] = df['Province/State'].fillna("")
         df['country'] = df['Country/Region'] + " " + df['Province/State']
         df['country'] = df['country'].str.rstrip()
-        #pivot the dataframe
+
+        # pivot the dataframe
         time_series = time_series.T
         time_series.columns = df['country']
-        #melt countries and deaths into single column
+
+        # melt countries and deaths into single column
         df_melted = pd.melt(time_series.reset_index(), id_vars=["index"])
-        df_melted.columns = ['date','country','deaths']
+        df_melted.columns = ['date', 'country', 'deaths']
         df_melted['date'] = pd.to_datetime(df_melted['date'])
-        
+
         return df_melted
 
-    def _get_apple_movement_indices(movement_type='walking'):
+    def _get_apple_movement_indices(self, movement_type='walking'):
         """
         get latest time series data from apple on population movement by country
             - movement_type <str> enum "walking"|"driving|"transit"
@@ -106,19 +110,22 @@ class CSVDataBase(DataBase):
             df = pd.read_csv(url)
         except:
             df = pd.read_csv("https://covid19-static.cdn-apple.com/covid19-mobility-data/2007HotfixDev47/v2/en-us/applemobilitytrends-2020-05-03.csv")
-        
-        meta_cols = ['geo_type','region','transportation_type']
-        #filter by movement type
+
+        meta_cols = ['geo_type', 'region', 'transportation_type']
+
+        # filter by movement type
         df_walk = df.loc[df['transportation_type'] == movement_type]
         assert df_walk['region'].is_unique
-        time_series = df_walk.drop(meta_cols,axis=1)
-        #pivot the dataframe
+        time_series = df_walk.drop(meta_cols, axis=1)
+
+        # pivot the dataframe
         time_series = time_series.T
         time_series.columns = df_walk['region']
-        #melt countries and y vals into single column
-        df_melted = pd.melt(time_series.iloc[:,:].reset_index(),id_vars=['index'])
-        df_melted.columns = ['date','country','movement_index']
-        df_melted['date'] = pd.to_datetime(df_melted['date'],errors='coerce')
+
+        # melt countries and y vals into single column
+        df_melted = pd.melt(time_series.iloc[:,:].reset_index(), id_vars=['index'])
+        df_melted.columns = ['date', 'country', 'movement_index']
+        df_melted['date'] = pd.to_datetime(df_melted['date'], errors='coerce')
         return df_melted
 
 if __name__ == '__main__':
